db29f371e45c8844c255658c669309738ddf8a6f
Jiaqi Chen <jackychen901010@gmail.com>
2015-04-30 17:21:10 -0500

    note

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -22,5 +22,64 @@ switch success {
 }
 
 
+protocol ExampleProtocol{
+//    var simpleDescription:String {get}
+    mutating func adjust()
+}
+
+class SimpleClass:ExampleProtocol {
+    var simpleDescription:String = "A very simple class."
+    var anotherProperty:Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+var a = SimpleClass();
+a.adjust()
+a.simpleDescription
+
+struct SimpleStruct:ExampleProtocol {
+    var simpleDescription:String = "A simple structure"
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+
+var b = SimpleStruct()
+b.adjust()
+let bDescription = b.simpleDescription
+
+enum simpleEnum:ExampleProtocol{
+    case one
+    case two
+    mutating func adjust() {
+        switch self{
+        case .one:
+            self = two
+        case .two:
+            self = one
+        }
+    }
+}
+
+protocol doubleProtocol{
+    var absoluteValue:Double {get}
+    mutating func circleArea()
+}
+
+extension Double:doubleProtocol{
+    var absoluteValue:Double{
+        return 3.1415926
+    }
+    mutating func circleArea() {
+        self = self*self*absoluteValue
+    }
+}
+
+var d:Double = 3.0
+d.circleArea()
+
+
+
 
 
